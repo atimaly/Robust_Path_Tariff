932d3f011a90ff5e659baf960b8442bf57fd38ad
atimaly <atimaly@gmail.com>
2023-03-14 13:28:35 +0100

    Some New Changes

--- build/CMakeFiles/robust_path_tariff.dir/DependInfo.cmake
+++ build/CMakeFiles/robust_path_tariff.dir/DependInfo.cmake
@@ -7,7 +7,7 @@ set(CMAKE_DEPENDS_CHECK_CXX
   "/home/anarchturandot/Documents/Lemon/lemon-main/contrib/Robust_Path_Tariff/src/main_cplex.cpp" "/home/anarchturandot/Documents/Lemon/lemon-main/contrib/Robust_Path_Tariff/build/CMakeFiles/robust_path_tariff.dir/src/main_cplex.cpp.o"
   "/home/anarchturandot/Documents/Lemon/lemon-main/contrib/Robust_Path_Tariff/src/robust_energy_cplex.cpp" "/home/anarchturandot/Documents/Lemon/lemon-main/contrib/Robust_Path_Tariff/build/CMakeFiles/robust_path_tariff.dir/src/robust_energy_cplex.cpp.o"
   )
-set(CMAKE_CXX_COMPILER_ID "Clang")
+set(CMAKE_CXX_COMPILER_ID "GNU")
 
 # Preprocessor definitions for this target.
 set(CMAKE_TARGET_DEFINITIONS_CXX
